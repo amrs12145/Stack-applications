69f9800e922a8f9e157d7f5df72c9c126eda50cb
Amr Samy <amrs12145@gmail.com>
2020-11-19 21:45:42 +0200

    From redmi

--- src/javaapplication2/Stack.java
+++ src/javaapplication2/Stack.java
@@ -39,6 +39,37 @@ public class Stack
     {
         return top==null;
     }
+    // By mobile
+
+
+int peek()
+{
+return top.data;
+}
+
+Stack tmp = new Stack();
+
+boollean(int data)
+{
+while( !isEmpty() )
+ {
+  if ( top.data == data )
+     return true;
+  else
+  {
+    tmp.push( pop() );
+  }
+ }
+
+while( tmp.top != null  )
+{
+push( tmp.pop() );
+}
+
+return false;
+}
+    
+
     
     
 }
